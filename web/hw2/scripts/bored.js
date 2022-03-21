df141f55cdc28eb96774c1a5b306ad7173deda2e
klhftco <klhftco@gmail.com>
2022-03-20 20:16:24 -0700

    hw2 submission

--- web/hw2/scripts/bored.js
+++ web/hw2/scripts/bored.js
@@ -2,11 +2,11 @@ const boredButton = document.getElementById("bored-button");
 const card = document.getElementById("card");
 
 // BEGIN PART 7
-
 async function fetchActivity() {
-  // YOUR CODE HERE
+  const response = await fetch("https://www.boredapi.com/api/activity");
+  const activity = await response.json();
+  return activity;
 }
-
 // END PART 7
 
 boredButton.addEventListener("click", async () => {
@@ -17,6 +17,10 @@ boredButton.addEventListener("click", async () => {
   card.replaceChildren();
   const activity = await fetchActivity();
   // BEGIN PART 8
-
+  for (const [key, value] of Object.entries(activity)) {
+    const detail = document.createElement("p");
+    detail.innerHTML = key + ": " + value;
+    card.appendChild(detail);
+  }
   // END PART 8
 });
