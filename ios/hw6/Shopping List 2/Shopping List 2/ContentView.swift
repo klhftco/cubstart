58d336a58df2e4d670b6506f816d81378b8f4389
klhftco <klhftco@gmail.com>
2022-06-08 19:46:59 -0700

    added all hw skletons

---
+++ ios/hw6/Shopping List 2/Shopping List 2/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Shopping List 2
+//
+//  Created by Tony Hong on 3/27/22.
+//
+
+import SwiftUI
+
+class Items: Identifiable {
+    
+    var itemName: String
+    
+    var quantity: String
+    
+    init(itemName: String, quantity: String) {
+        
+        self.itemName = itemName
+        
+        self.quantity = quantity
+    }
+    
+}
+
+struct ContentView: View {
+    
+    var list = [
+        Items(itemName: "Bananas", quantity: "3"),
+        Items(itemName: "Apples", quantity: "4"),
+        Items(itemName: "Eggs", quantity: "12")
+    ]
+        
+    var body: some View {
+        NavigationView {
+            List {
+            }.navigationTitle("Shopping List")
+        }
+    }
+}
